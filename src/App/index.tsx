bc69d80a0fd5d01d6dfbdaaac41e780f8e590c5d
ale917k <alessiopetrin9@gmail.com>
2021-06-23 21:22:02 +0100

    Setup index.html + Added ESLint & Prettier rules
    - Updated index.html & manifest.json with weather forecast title and information
    - Added ESLint and Prettier rules & formatted codebase
    - Removed unnecessary CRA starter code

---
+++ src/App/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Wrapper from "./styles";
+
+/**
+ * Main sections and routes.
+ * @returns App routes.
+ */
+const App: React.FC = () => <Wrapper>App Content</Wrapper>;
+
+export default App;
